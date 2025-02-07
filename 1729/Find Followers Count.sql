6d020462ee00b5cdc8703c3bdf0055a4d3db3428
yahngs <subham.malakar149@gmail.com>
2025-02-07 12:52:13 +0530

    Create Find Followers Count.sql

---
+++ 1729/Find Followers Count.sql
@@ -0,0 +1,12 @@
+-- Write a solution that will, for each user, return the number of followers.
+-- Return the result table ordered by user_id in ascending order.
+
+select 
+    user_id, 
+    count(follower_id) as followers_count
+from 
+    followers
+group by 
+    user_id
+order by 
+    user_id
