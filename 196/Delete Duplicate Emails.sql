9ce2c84ab9482617cfca285b3db7b3fc9fdc87e2
yahngs <subham.malakar149@gmail.com>
2025-02-07 13:51:20 +0530

    Create Delete Duplicate Emails.sql

---
+++ 196/Delete Duplicate Emails.sql
@@ -0,0 +1,20 @@
+/*
+Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+For Pandas users, please note that you are supposed to modify Person in place.
+
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. 
+The final order of the Person table does not matter.
+*/
+
+DELETE
+FROM
+person
+WHERE Id NOT IN
+(
+SELECT minid
+FROM
+(SELECT email, min(id) as minid
+FROM Person
+GROUP BY email ) test)
